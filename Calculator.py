18fb98585818753a0da4c043a2276d93f90a7620
Williamtobs <63688314+Williamtobs@users.noreply.github.com>
2020-04-25 18:20:41 +0100

    Update and rename .gitignore to Calculator.py

---
+++ Calculator.py
@@ -0,0 +1,20 @@
+#first calculator code using python
+
+print('Calculator code')
+print('---------------------------')
+#user input
+a=int(input('Enter first number: '))
+b=input('Enter +,-,/,*: ')
+c=int(input('Enter second number: '))
+
+if(b=='+'):
+print(a+c)
+elif(b=='-'):
+print(a-c)
+elif(b=='/'):
+print(a/c)
+elif(b==''):
+print(ac)
+else:
+print('insert +,-,/,* sign')
+
